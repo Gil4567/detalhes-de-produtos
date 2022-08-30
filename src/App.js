5f7f871a71238119e4dd1346ff51cda3febc9711
Gilberto Carlos <gilberto8207@hotmail.com>
2022-08-29 21:20:35 -0300

    subindo arquivos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  state = {
+    products: [
+      {
+        "_id": "1",
+        "title": "Detalhe do produto",
+        "src": [
+  
+            "https://github.com/mfugissecruz/hackadev-fashion-shop/blob/main/images/index/product12.jpg?raw=true",
+            "https://github.com/mfugissecruz/hackadev-fashion-shop/blob/main/images/index/product13.jpg?raw=true",
+            "https://github.com/mfugissecruz/hackadev-fashion-shop/blob/main/images/index/product10.jpg?raw=true",
+            "https://github.com/mfugissecruz/hackadev-fashion-shop/blob/main/images/index/product15.jpg?raw=true"
+          ],
+        "description": "UI/UX designing, html css tutorials",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 23,
+        "colors":["red","black","crimson","teal"],
+        "count": 1
+      }
+    ],
+    index: 0
+  };
+
+  myRef = React.createRef();
+
+  handleTab = index =>{
+    this.setState({index: index})
+    const images = this.myRef.current.children;
+    for(let i = 0; i < images.length; i++){
+      images[i].className = images[i].className.replace("active", "")
+    }
+    images[index].className = "active";
+  }
+
+  render(){
+    const {products, index} = this.state;
+    return(
+      <div className="app">
+      {
+        products.map(item => (
+          <div className="detalhes" key={item._id}>
+            <div className="big-img">
+              <img src={item.src[index]} alt=""/>
+            </div>
+            <div className="box">
+              <div className="row">
+                <h2>{item.title}</h2>
+                <span>${item.price}</span>
+              </div>
+              <div className="colors">
+                {
+                  item.colors.map((color, index) =>(
+                    <button style={{background: color}} key={index}></button>
+                  ))
+                }
+              </div>
+              <p>{item.description}</p>
+              <p>{item.content}</p>
+
+              <div className="thumb" ref={this.myRef}>
+                {
+                  item.src.map((img, index) =>(
+                    <img src={img} alt="" key={index}
+                    onClick={() => this.handleTab(index)}
+                    />
+                  ))
+                }
+              </div>
+              <button className="cart">Adicionar ao carrinho</button>
+
+            </div>
+          </div>
+        ))
+      }
+      </div>
+    );
+  };
 }
 
 export default App;
