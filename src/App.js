2d651d44d8ed1f337433d3b27c35cef50a59e771
Gilberto carlos <gilberto8207@hotmail.com>
2022-09-25 01:07:14 -0300

    conectado com sucesso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import api from './services/api';
 
+class App extends Component{
 
-
-function App() {
-
-  const [buscaProdutos, setProdutos] = useState([]);
-
-  useEffect(() => {
+  state = {
+    produtos: [],
     
-    api.get('product/7').then(({data}) => {
-      setProdutos(data)
-    });
-    console.log(buscaProdutos)
-  }, ['']);
+  }
+
+  async componentDidMount(){
+    const response = await api.get('/product/2');
+    
+
+    this.setState({produtos: response.data});
+  }
 
 
-  
-}
+  render(){
+
+    const {produtos} = this.state;
+    console.log(produtos);
+
+    return(
+      <div className="detalhes" key={produtos.id}>
+            <div className="big-img">
+              <img src={produtos.image} alt=""/>
+            </div>
+            <div className="box">
+              <div className="row">
+                <h2>{produtos.product}</h2>
+                <span>${produtos.price}</span>
+              </div>
+              <div className="colors">
+                
+              </div>
+              <p>{produtos.description}</p>
+              <p>{produtos.content}</p>
+
+              <div className="thumb" ref={this.myRef}>
+                
+              </div>
+              <button className="cart">Adicionar ao carrinho</button>
+
+            </div>
+          </div>
+    );
+  };
+
+};
 
 export default App;
